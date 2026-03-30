dfe61465816d41a81278bde026219acd007d22e4
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 23:32:37 +0200

    Fix typo in docstring and adjust import statement formatting in np_cat function

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 """ Fucntion that concatenate two matrices algon specifics axes"""
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """ Concatenates two matrices along a specific axis"""
+    """ Concatenates two matrices along a specific axis """
     return np.concatenate((mat1, mat2), axis=axis)
