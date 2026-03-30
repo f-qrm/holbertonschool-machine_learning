4e06a5c25bb91a81d94f59164dd11b3cb475edfc
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 23:38:28 +0200

    Add np_matmul function for matrix multiplication and main script for testing

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that performs matrix multiplication """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Matrix multiplication """
+    return np.matmul(mat1, mat2)
