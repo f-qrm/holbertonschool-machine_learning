be8b4afb79498326ba2e81ebc042984bd17b6541
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 23:22:47 +0200

    Add np_elementwise function and main script for element-wise operations

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that performs element-wise addition, subtraction,
+    multiplication, and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """ Adittion, Subtraction, Multiplication and Division """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
