f17f766cce54e17a84423856790dc1cf4c82cf36
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 22:59:03 +0200

    Add np_shape function and main script for testing numpy.ndarray shape

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that calculates the shape of a numpy.ndarray """
+
+
+def np_shape(matrix):
+    """ The shape of matrix"""
+    np_shape = matrix.shape
+    return np_shape
