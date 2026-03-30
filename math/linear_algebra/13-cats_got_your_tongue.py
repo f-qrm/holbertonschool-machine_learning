2928c8cae7ecc70f49609188ed62e0e5242d28af
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 23:31:29 +0200

    Add np_cat function for matrix concatenation and main script for testing

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+""" Fucntion that concatenate two matrices algon specifics axes"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ Concatenates two matrices along a specific axis"""
+    return np.concatenate((mat1, mat2), axis=axis)
