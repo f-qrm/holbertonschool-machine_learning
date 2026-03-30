e040e1f153e394636baac36376290219a869c58c
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 23:14:19 +0200

    Add np_transpose function for matrix transposition and main script for testing

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that transposes matrix """
+
+
+def np_transpose(matrix):
+    """transpose matrix"""
+    np_transpose = matrix.transpose()
+    return np_transpose
