9b49f36470f4de1386437be0ef2589f58b7ff984
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 19:54:49 +0200

    Add matrix_shape function and example usage in 2-main.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,9 @@
+def matrix_shape(matrix):
+
+    element_courant = matrix
+    matrix_shape = []
+
+    while isinstance(element_courant, list):
+        matrix_shape.append(len(element_courant))
+        element_courant = element_courant[0]
+    return matrix_shape
