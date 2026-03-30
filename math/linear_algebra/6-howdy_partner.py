bde5bd564c79462cd7d2aac02a5196b23357ad2f
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 21:51:10 +0200

    Add cat_arrays function and main script for array concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that concatenates two arrays """
+
+
+def cat_arrays(arr1, arr2):
+    """ Concatenates two arrays """
+    cat_arrays = arr1 + arr2
+    return cat_arrays
