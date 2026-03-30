5d004784fc8d335571b1e06a57ff72b4b39dbcfb
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 20:01:49 +0200

    more docstring for the checker

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""Function that calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
 
-    """Function that calculates the shape of a matrix"""
+    """Returns the shape of a matrix as a list of integers"""
 
     element_courant = matrix
     matrix_shape = []
