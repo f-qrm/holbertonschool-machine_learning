50972af7a37b38249b3d9aaaf02006d56a6a35e2
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-03-30 21:41:49 +0200

    Add add_matrices2D function and main script for element-wise addition of matrices

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Function that adds two matrices element-wise """
+
+
+def add_matrices2D(mat1, mat2):
+    """ Adds 2D matrices """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+
+    add_matrices2D = [[0] * len(mat1[0]) for _ in range(len(mat2))]
+
+    for i in range(len(mat1[0])):
+        for j in range(len(mat2)):
+            add_matrices2D[i][j] = mat1[i][j] + mat2[i][j]
+    return add_matrices2D
