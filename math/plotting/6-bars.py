4c1f7e86bebe73180c36e8a72ab0244f26026e1a
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-07 00:09:10 +0200

    Fix typo in x-ticks labels for stacked bar graph

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -18,7 +18,7 @@ def bars():
             bottom=fruit[0] + fruit[1])
     plt.bar(x, fruit[3], width=0.5, color='#ffe5b4', label='peaches',
             bottom=fruit[0] + fruit[1] + fruit[2])
-    plt.xticks(x, ['Farah', 'Fred', 'Felicia'])
+    plt.xticks(x, ['Farrah', 'Fred', 'Felicia'])
     plt.yticks(np.arange(0, 81, 10))
     plt.ylim(0, 80)
     plt.ylabel("Quantity of Fruit")
