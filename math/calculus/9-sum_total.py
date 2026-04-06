2c9dab5e0f430ead9588e2ba5e2196d4676084f3
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-07 01:59:30 +0200

    Fix formatting in summation_i_squared function for consistency

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,5 +6,5 @@ def summation_i_squared(n):
     """function def summation_i_squared(n): that calculates n!"""
     if not isinstance(n, int) or isinstance(n, bool):
         return None
-    result = n* (n + 1) * (2 * n + 1) // 6
+    result = n * (n + 1) * (2 * n + 1) // 6
     return result
