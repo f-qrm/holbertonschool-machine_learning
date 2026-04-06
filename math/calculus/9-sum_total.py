aa6b3190c2b73dff0ba21695c7230e271b409271
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-07 01:51:57 +0200

    Fix type check in summation_i_squared function to exclude boolean values

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def summation_i_squared(n):
     """function def summation_i_squared(n): that calculates n!"""
-    if not isinstance(n, int):
+    if not isinstance(n, int) or isinstance(n, bool):
         return None
     return int(np.sum(np.arange(1, n + 1) ** 2))
