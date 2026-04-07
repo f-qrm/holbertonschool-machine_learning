2297f7a792d8bfe9a78a6abaf51b06b50986e2b0
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-07 10:44:02 +0200

    pycodestyle

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,8 +4,8 @@
 
 def poly_integral(poly, C=0):
     """function that calculates the integral of a polynomial"""
-    if not isinstance(poly, list) or len(poly) == 0
-    or not isinstance(C, (int, float)):
+    if not isinstance(poly, list) or len(poly) == 0 \
+       or not isinstance(C, (int, float)):
         return None
     inter = [C]
     for i in range(0, len(poly)):
