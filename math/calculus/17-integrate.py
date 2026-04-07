ebcfcd0d810b8844b249f509e62aec7fb6e1f552
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-07 10:40:48 +0200

    Add initial implementation of poly_integral function

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Computes the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """function that calculates the integral of a polynomial"""
+    if not isinstance(poly, list) or len(poly) == 0
+    or not isinstance(C, (int, float)):
+        return None
+    inter = [C]
+    for i in range(0, len(poly)):
+        coef = (poly[i] / (i + 1))
+        if coef % 1 == 0:
+            coef = int(coef)
+        inter.append(coef)
+    return inter
