30f034c07c3df17b2898dee33ad0ca1cf537fe1f
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-20 09:04:00 +0200

    Add Neuron class implementation for binary classification

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Class that defines a single neuron performing classification """
+import numpy as np
+
+
+class Neuron:
+    """ Class that defines a single neuron performing classification """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Return the weight """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Return the bias """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Return the prediction """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Calculate the forward propagation of the neuron """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
