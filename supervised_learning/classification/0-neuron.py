c9a3763dc50ac53d02de4169a2f1858917100031
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-20 07:54:59 +0200

    Add neuron class implementation and data visualization scripts for binary classification

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Class that defines a single neuron performing classification """
+import numpy as np
+
+
+class Neuron:
+    """ Class that defines a single neuron performing classification """
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
