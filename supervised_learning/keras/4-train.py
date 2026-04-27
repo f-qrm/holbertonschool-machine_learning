3c4d5d232581c188f41140081892bad824f5f9c4
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-28 00:50:11 +0200

    Add main script and training module for Keras model

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that trains a model using mini-batch gradient descent """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
+    """ Function that trains a model using mini-batch gradient descent """
+    return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                       verbose=verbose, shuffle=shuffle)
