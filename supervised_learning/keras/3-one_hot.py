67011b06dba5a0b16310ad5b4fb03a294e90241b
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-28 00:19:00 +0200

    Fix formatting in one_hot function docstring

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -2,6 +2,7 @@
 """Module that converts a label vector into a one-hot matrix"""
 import tensorflow.keras as K
 
+
 def one_hot(labels, classes=None):
-    """ Function that converts a label vector into a one-hot matrix"""
+    """Function that converts a label vector into a one-hot matrix"""
     return K.utils.to_categorical(labels, num_classes=classes)
