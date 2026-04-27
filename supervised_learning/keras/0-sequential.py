70285ff7c13349639a1e0503859fd84ed7c4491e
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-27 11:09:43 +0200

    Refactor build_model to add InputLayer and streamline input handling

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,12 +6,19 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """function that builds a neural network with the Keras library"""
     model = K.Sequential()
-    model.add(K.layers.InputLayer(shape=(nx,)))
     for i in range(len(layers)):
-        model.add(K.layers.Dense(
-            layers[i],
-            activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha),
+        if i == 0:
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha),
+                input_shape=(nx,)
+            ))
+        else:
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha),
             ))
         if i != len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
