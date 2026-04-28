9297652226959499e95ab122c99467aaae70e9c5
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-28 13:35:02 +0200

    Add main script and prediction module for Keras model inference

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module that makes a prediction using a neural network """
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """ Function that makes a prediction using a neural network """
+    return network.predict(data, verbose=verbose)
