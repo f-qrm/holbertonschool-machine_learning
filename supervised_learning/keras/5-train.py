5a2e7d55a98084bc89409a230229094cdde47545
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-28 09:24:29 +0200

    Fix formatting in train_model function for better readability

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -7,4 +7,5 @@ def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False, validation_data=None):
     """ Function that trains a model using mini-batch gradient descent """
     return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
-                       verbose=verbose, shuffle=shuffle, validation_data=validation_data)
+                       verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data)
