13bea893dfa16154596f7f64d671b14c74ca10a8
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-04-28 09:34:34 +0200

    Add main script and training module for Keras model training

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module that trains a model using mini-batch gradient descent """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, learning_rate_decay=False, alpha=0.1,
+                decay_rate=1, verbose=True, shuffle=False):
+    """ Function that trains a model using mini-batch gradient descent """
+    callbacks = []
+    if early_stopping and validation_data:
+        erl_stp = K.callbacks.EarlyStopping(monitor='val_loss',
+                                            patience=patience)
+        callbacks.append(erl_stp)
+    if learning_rate_decay and validation_data:
+        def schedule(epoch):
+            """Function that computes the learning rate for each epoch"""
+            return alpha / (1 + decay_rate * epoch)
+        lr_decay = K.callbacks.LearningRateScheduler(schedule, verbose=1)
+        callbacks.append(lr_decay)
+    return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                       verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data, callbacks=callbacks)
