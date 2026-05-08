cf9c0555041fe292248748a0051098aeb497cdd3
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-05-08 15:24:45 +0200

    Add batch normalization layer and main script for testing

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for creating a batch normalization layer in TensorFlow."""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """Create a batch normalization layer for a neural network in TensorFlow.
+
+    Args:
+        prev: The activated output of the previous layer.
+        n (int): The number of nodes in the layer to be created.
+        activation: The activation function for the output of the layer.
+
+    Returns:
+        tensor: The activated output for the layer.
+    """
+    layer = tf.keras.layers.Dense(n, kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'))
+    Z = layer(prev)
+    bn_layer = tf.keras.layers.BatchNormalization(epsilon=1e-7)
+    Z_norm = bn_layer(Z, training=True)
+    return activation(Z_norm)
