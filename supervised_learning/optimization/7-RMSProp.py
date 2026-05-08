bb6f4ccee84177cbd5e9496aefdb174de3787603
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-05-08 10:25:56 +0200

    Add RMSProp optimization module and main script for model training

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for updating variables using the RMSProp optimization algorithm."""
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Update a variable using the RMSProp optimization algorithm.
+
+        Args:
+            alpha (float): The learning rate.
+            beta2 (float): The RMSProp weight.
+            epsilon (float): Small number to avoid division by zero.
+            var (numpy.ndarray): Variable to be updated.
+            grad (numpy.ndarray): Gradient of var.
+            s (numpy.ndarray): Previous second moment of var.
+
+        Returns:
+            tuple: The updated variable and the new moment, respectively.
+    """
+    s = beta2 * s + (1 - beta2) * grad**2
+    var = var - (alpha / np.sqrt(s + epsilon) * grad)
+    return var, s
