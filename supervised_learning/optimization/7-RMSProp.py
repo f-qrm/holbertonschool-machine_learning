Fix variable update calculation in RMSProp optimization

supervised_learning/optimization/7-RMSProp.py
@@ -18,5 +18,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
             tuple: The updated variable and the new moment, respectively.
     """
     s = beta2 * s + (1 - beta2) * grad**2
-    var = var - (alpha / np.sqrt(s + epsilon) * grad)
+    var = var - alpha * (grad / np.sqrt(s + epsilon))
     return var, s