9a710a9cb4011093da52ea58f5d2da3264bc7679
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-05-13 07:33:40 +0200

    Refactor L2 regularization layer creation to improve readability and maintainability

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,9 +5,12 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Creates a neural network layer with L2 regularization """
+    initializer = tf.keras.initializer.VarianceScaling(
+        scale=2.0, mode='fan_avg')
     layer = tf.keras.layers.Dense(
-        n,
+        units=n,
         activation=activation,
+        kernel_initializer=initializer,
         kernel_regularizer=tf.keras.regularizers.L2(lambtha)
     )
     return layer(prev)
