93693f20f3f7d01afa0f50ffd14404e5d4e3edb7
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-05-13 07:35:18 +0200

    Fix typo in TensorFlow initializers for L2 regularization layer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Creates a neural network layer with L2 regularization """
-    initializer = tf.keras.initializer.VarianceScaling(
+    initializer = tf.keras.initializers.VarianceScaling(
         scale=2.0, mode='fan_avg')
     layer = tf.keras.layers.Dense(
         units=n,
