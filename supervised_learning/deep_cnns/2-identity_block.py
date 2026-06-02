df4ddc32bc56e71ea9e5f08106607ea40d7e8b7c
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-06-02 08:40:42 +0200

    Refactor identity block to use a single initializer variable for Conv2D layers

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -14,19 +14,17 @@ def identity_block(A_prev, filters):
         activated output of the identity block
     """
     F11, F3, F12 = filters
+    init = K.initializers.HeNormal(seed=0)
     conv = K.layers.Conv2D(F11, (1, 1), padding='same',
-                           kernel_initializer=K.initializers.HeNormal(seed=0))\
-    (A_prev)
+                           kernel_initializer=init)(A_prev)
     bn = K.layers.BatchNormalization(axis=3)(conv)
     act = K.layers.Activation('relu')(bn)
     conv1 = K.layers.Conv2D(F3, (3, 3), padding='same',
-                            kernel_initializer=K.initializers.HeNormal(seed=0))\
-    (act)
+                            kernel_initializer=init)(act)
     bn1 = K.layers.BatchNormalization(axis=3)(conv1)
     act1 = K.layers.Activation('relu')(bn1)
     conv2 = K.layers.Conv2D(F12, (1, 1), padding='same',
-                            kernel_initializer=K.initializers.HeNormal(seed=0))\
-    (act1)
+                            kernel_initializer=init)(act1)
     bn2 = K.layers.BatchNormalization(axis=3)(conv2)
     add = K.layers.Add()([bn2, A_prev])
     act2 = K.layers.Activation('relu')(add)
