74cb0ebc31b7d7b49cba8c4f6e65ec761f27d99d
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-06-25 11:48:33 +0200

    Fix formatting of PDF calculation in Normal class for better readability

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -71,5 +71,6 @@ class Normal:
         """
         e = 2.7182818285
         pi = 3.1415926536
-        pdf = (1 / (self.stddev * (2 * pi)** 0.5)) * e ** (-0.5 * self.z_score(x) ** 2)
+        pdf = (1 / (self.stddev * (2 * pi) ** 0.5)) * (
+            e ** (-0.5 * self.z_score(x) ** 2))
         return pdf
