0b3d01e489ecf25ddff8ba7e64e048bc6519dbf7
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-06-25 12:03:45 +0200

    Add CDF method to Normal class and create main script for testing

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -74,3 +74,19 @@ class Normal:
         pdf = (1 / (self.stddev * (2 * pi) ** 0.5)) * (
             e ** (-0.5 * self.z_score(x) ** 2))
         return pdf
+
+    def cdf(self, x):
+        """Calculates the CDF value for a given x-value.
+
+            Args:
+                x (float): x-value.
+
+            Returns:
+                float: CDF value for x.
+        """
+        pi = 3.1415926536
+        z = (x - self.mean) / (self.stddev * 2 ** 0.5)
+        erf = (2 / (pi ** 0.5)) * (z - ((z**3) / 3) + ((z**5) / 10)
+                                   - ((z**7) / 42) + ((z**9) / 216))
+        cdf = 0.5 * (1 + erf)
+        return cdf
