c1060666767680b3ad1b9c8f479c3872ea31b777
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-07-16 13:48:30 +0200

    Refactor return statement in HP function to use np.squeeze for clarity

--- unsupervised_learning/dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/dimensionality_reduction/3-entropy.py
@@ -31,4 +31,4 @@ def HP(Di, beta):
     # Shannon entropy of the resulting distribution
     Hi = np.log2(summ) + (beta * np.sum(Di * j_weight) / summ) / np.log(2)
 
-    return (np.asarray(Hi).item(), Pi)
+    return (np.squeeze(Hi), Pi)
