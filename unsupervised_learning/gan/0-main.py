09f805bbf6dd2dead7ffcf7576cbfad447b805b5
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-08-12 17:52:02 +0200

    feat: add convolutional GAN generator and discriminator models

--- unsupervised_learning/gan/0-main.py
+++ unsupervised_learning/gan/0-main.py
@@ -2,12 +2,12 @@
 
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-import random
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import random
 
-WGAN_clip = __import__('1-wgan_clip').WGAN_clip
-
+convolutional_GenDiscr = __import__('3-generate_faces').convolutional_GenDiscr
+Simple_GAN = __import__('0-simple_gan').Simple_GAN
 
 ## Regulating the seed
 
@@ -16,157 +16,68 @@ def set_seeds(seed) :
     np.random.seed(seed)
     tf.random.set_seed(seed)
 
-
-def hash_tensor(tensor) :
-    return np.sum(np.array([hash(x)%2**30 for x in tensor.numpy().flatten()]))%2**30
-
-
 def spheric_generator(nb_points, dim) :
     u=tf.random.normal(shape=(nb_points, dim))
     return u/tf.reshape(tf.sqrt(tf.reduce_sum(tf.square(u),axis=[1])+10**-8),[nb_points,1])
 
-def normal_generator(nb_points, dim) :
-    return tf.random.normal(shape=(nb_points, dim))
+## Load the pictures from small_res_faces_10000
 
-def uniform_generator(nb_points, dim) :
-    return tf.random.uniform(shape=(nb_points, dim))
+array_of_pictures=np.load("small_res_faces_10000.npy")
+array_of_pictures=array_of_pictures.astype("float32")/255
 
+## Center and Normalize the data
 
-## Building Generator and Discriminator
-
-def fully_connected_GenDiscr(gen_shape, real_examples, latent_type="normal" ) :
-
-    #   Latent generator
-    if latent_type   == "uniform" :
-        latent_generator  =  lambda k : uniform_generator(k,gen_shape[0])
-    elif latent_type == "normal" :
-        latent_generator  =  lambda k : normal_generator(k,gen_shape[0])
-    elif latent_type == "spheric" :
-        latent_generator  =  lambda k : spheric_generator(k,gen_shape[0])
-
-    #   Generator
-    inputs     = keras.Input(shape=( gen_shape[0] , ))
-    hidden     = keras.layers.Dense( gen_shape[1] , activation = 'tanh'    )(inputs)
-    for i in range(2,len(gen_shape)-1) :
-        hidden = keras.layers.Dense( gen_shape[i] , activation = 'tanh'    )(hidden)
-    outputs    = keras.layers.Dense( gen_shape[-1], activation = 'sigmoid' )(hidden)
-    generator  = keras.Model(inputs, outputs, name="generator")
-
-    #   Discriminator
-    inputs        = keras.Input(shape=( gen_shape[-1], ))
-    hidden        = keras.layers.Dense( gen_shape[-2],   activation = 'tanh' )(inputs)
-    for i in range(2,len(gen_shape)-1) :
-        hidden    = keras.layers.Dense( gen_shape[-1*i], activation = 'tanh' )(hidden)
-    outputs       = keras.layers.Dense( 1 ,              activation = 'tanh' )(hidden)
-    discriminator = keras.Model(inputs, outputs, name="discriminator")
-
-    return generator, discriminator, latent_generator
-
-# illustration:
-def illustr_fully_connected_GenDiscr() :
-    generator, discriminator, latent_generator = fully_connected_GenDiscr([1,100,100,2], None)
-    print(generator.summary())
-    print(discriminator.summary())
-
-
-## Training a fully connected GAN (Simple GAN)
-
-def example_fully_connected_GAN(chosen_type ,real_examples, gen_shape, epochs,  batch_size=200, steps_per_epoch=250, latent_type="normal", learning_rate=.005):
-    generator, discriminator, latent_generator = fully_connected_GenDiscr(gen_shape, real_examples, latent_type=latent_type )
-    G=chosen_type( generator , discriminator , latent_generator, real_examples, learning_rate=learning_rate)
-    G.compile()
-    G.fit(real_examples, epochs=epochs, steps_per_epoch=steps_per_epoch, verbose=1)
-    return G
-
+mean_face=array_of_pictures.mean(axis=0)
+centered_array = array_of_pictures - mean_face
+multiplier=np.max(np.abs(array_of_pictures),axis=0)
+normalized_array = centered_array/multiplier
 
 ## An example of a large real sample
 
-def four_clouds_example(N_real):
-    X   = np.random.randn( N_real)*.05
-    Y   = np.random.randn( N_real)*.05
-    X[:N_real//2]+=.75
-    X[N_real//2:]+=.25
-    Y[N_real//4:N_real//2]+=.25
-    Y[:N_real//4]+=.75
-    Y[N_real//2:3*N_real//4]+=.75
-    Y[3*N_real//4:]+=.25
-    R   = np.minimum(X*X,1)
-    G   = np.minimum(Y*Y,1)
-    B   = np.maximum(1-R-G,0)
-    return tf.convert_to_tensor(np.vstack([X,Y,R,G,B]).T)
+lat_gen = lambda k : spheric_generator(k, 16)*.3+.5
+real_ex = tf.convert_to_tensor(normalized_array,dtype="float32")
 
+## Visualize the faces
 
-## Visualize 5D the result of G
+def recover(normalized) :
+    return normalized*multiplier+mean_face
 
-def visualize_5D(G, show=True, title=None, filename=None, dpi=200):
-    fig, axes = plt.subplots(2, 3, figsize=(12, 8))
-    for ax in axes.flatten():
-        ax.set_xlim(-.1, 1.1)
-        ax.set_ylim(-.1, 1.1)
+def visualize_faces(G, lat_vecs, show_history=True):
+    fig = plt.figure(figsize=(20, 4))
+    wax = 4
+    gs = gridspec.GridSpec(wax * 2, wax * 10)
+    axes1 = [fig.add_subplot(gs[:wax - 1, i * wax:(i + 1) * wax - 1]) for i in range(10)]
+    axes2 = [fig.add_subplot(gs[wax:, 0:3 * wax]), fig.add_subplot(gs[wax:, 3 * wax + 1:6 * wax + 1]),
+             fig.add_subplot(gs[wax:, 6 * wax + 2:9 * wax + 3])]
 
-    X_tf = G.real_examples
-    X = X_tf.numpy()
-    axes[0, 0].scatter(X[:, 0], X[:, 1], c=X[:, 2:], s=1)
-    axes[0, 0].set_title("real")
+    generated_images = recover(G.generator(lat_vecs))
 
-    lat = G.latent_generator(10000)
-    Y_tf = G.generator(lat)
-    Y = Y_tf.numpy()
-    axes[0, 1].scatter(Y[:, 0], Y[:, 1], c=Y[:, 2:], s=1)
-    axes[0, 1].set_title("fake = generator(latent sample) ")
+    for i in range(10):
+        axes1[i].axis("off")
+        axes1[i].imshow(generated_images[i, :, :, 0])
 
-    cX = G.discriminator(X_tf).numpy()
-    cY = G.discriminator(Y_tf).numpy()
-    m = min(np.min(cX), np.min(cY))
-    M = max(np.max(cX), np.max(cY))
+    b = G.discriminator(G.get_fake_sample(400)).numpy()
+    a = G.discriminator(G.get_real_sample(400)).numpy()
+    axes2[0].hist(a, bins=20)
+    axes2[0].set_title("values of disc on real")
+    axes2[1].hist(b, bins=20)
+    axes2[1].set_title("values of disc on fake")
+    if show_history:
+        axes2[2].plot(G.history.history["discr_loss"])
+        axes2[2].plot(G.history.history["gen_loss"])
+        axes2[2].set_title("losses")
 
-    axes[1, 0].scatter(X[:, 0], X[:, 1], c=cX, s=1, vmin=m, vmax=M)
-    axes[1, 0].set_title("discriminator(real)")
-
-    Y = G.generator(G.latent_generator(10000)).numpy()
-    axes[1, 1].scatter(Y[:, 0], Y[:, 1], c=cY, s=1, vmin=m, vmax=M)
-    axes[1, 1].set_title("discriminator(fake)")
-
-    for i in range(2):
-        axes[i, 2].set_xlim(-3, 3)
-        axes[i, 2].set_ylim(-3, 3)
-
-    A = np.linspace(-3, 3, 150)
-    B = np.linspace(-3, 3, 150)
-    X, Y = np.meshgrid(A, B)
-    U = tf.convert_to_tensor(np.vstack([X.ravel(), Y.ravel()]).T)
-
-    X = G.discriminator(G.generator(U)).numpy()
-    u = axes[0, 2].pcolormesh(A, B, X[:, 0].reshape([150, 150]), shading='gouraud')
-
-    axes[0, 2].set_title(r"discriminator $\circ$ generator on latent space")
-
-    axes[1, 2].scatter(lat.numpy()[:, 0], lat.numpy()[:, 1], c=cY, s=1)
-    axes[1, 2].set_title(r"discriminator $\circ$ generator on latent sample")
-    # sci = lambda x : "{:.2E}".format(.99*x*(x>0)+1.01*x*(x<0))
-    # cb=fig.colorbar(u, ax=axes[1])
-    ##cb.set_ticks([sci(np.min(X[:,0])), 0, sci(np.max(X[:,0]))])
-    # cb.ax.tick_params(labelsize=5)
-
-    # some more tuning:
-    for a in axes.flatten():
-        a.tick_params(axis='both', which='major', labelsize=5)
-        a.tick_params(axis='both', which='minor', labelsize=5)
-
-    if title:
-        fig.suptitle(title)
-
-    if filename:
-        plt.savefig(filename, dpi=dpi)
-        if show:
-            plt.show()
-        plt.close()
-
-    elif show:
-        plt.show()
+    plt.show()
 
 ## LET'S GO !
 
+# Train
 set_seeds(0)
-G = example_fully_connected_GAN(WGAN_clip ,four_clouds_example(1000), [2,10,10,5], 16, steps_per_epoch=100, learning_rate=.001)
-visualize_5D(G,show=True, title=f"after 16 epochs")
+gen, discr = convolutional_GenDiscr()
+G=Simple_GAN(gen,discr, lat_gen, real_ex, disc_iter=1, batch_size=10,learning_rate=.001)
+G.compile()
+G.fit(real_ex,epochs=5,steps_per_epoch=5)
+
+# Visualize
+lat_vecs = lat_gen(10)
+visualize_faces(G,lat_vecs)
---
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+from tensorflow import keras
+
+
+def convolutional_GenDiscr():
+
+
+    def get_generator():
+        inputs = keras.Input(shape=(16,))
+        dense = keras.layers.Dense(2048, activation='tanh')(inputs)
+        reshaped = keras.layers.Reshape((2, 2, 512))(dense)
+        up1 = keras.layers.UpSampling2D()(reshaped)
+        conv1 = keras.layers.Conv2D(64, kernel_size=3, padding='same')(up1)
+        bn1 = keras.layers.BatchNormalization()(conv1)
+        act1 = keras.layers.Activation("tanh")(bn1)
+        up2 = keras.layers.UpSampling2D()(act1)
+        conv2 = keras.layers.Conv2D(16, kernel_size=3, padding="same")(up2)
+        bn2 = keras.layers.BatchNormalization()(conv2)
+        act2 = keras.layers.Activation(activation="tanh")(bn2)
+        up3 = keras.layers.UpSampling2D()(act2)
+        conv3 = keras.layers.Conv2D(1, kernel_size=3, padding="same")(up3)
+        bn3 = keras.layers.BatchNormalization()(conv3)
+        act3 = keras.layers.Activation(activation="tanh")(bn3)
+        gen = keras.Model(inputs, act3, name="generator")
+        return gen
+
+
+    def get_discriminator():
+        #discriminator model
+        inputs = keras.Input(shape=(16, 16, 1))
+        conv1 = keras.layers.Conv2D(32, kernel_size=3, padding="same")(inputs)
+        mp1 = keras.layers.MaxPool2D()(conv1)
+        act1 = keras.layers.Activation(activation="tanh")(mp1)
+        conv2 = keras.layers.Conv2D(64, kernel_size=3, padding="same")(act1)
+        mp2 = keras.layers.MaxPool2D()(conv2)
+        act2 = keras.layers.Activation(activation="tanh")(mp2)
+        conv3 = keras.layers.Conv2D(128, kernel_size=3, padding="same")(act2)
+        mp3 = keras.layers.MaxPool2D()(conv3)
+        act3 = keras.layers.Activation(activation="tanh")(mp3)
+        conv4 = keras.layers.Conv2D(256, kernel_size=3, padding="same")(act3)
+        mp4 = keras.layers.MaxPool2D()(conv4)
+        act4 = keras.layers.Activation(activation="tanh")(mp4)
+        flat = keras.layers.Flatten()(act4)
+        dense = keras.layers.Dense(1, activation="tanh")(flat)
+        discr = keras.Model(inputs, dense, name="discriminator")
+        return discr
+
+    return get_generator() , get_discriminator()
