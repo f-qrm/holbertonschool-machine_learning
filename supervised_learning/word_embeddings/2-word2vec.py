ed71344c947a4ab3aba1e234d4b3d38aab76e1f3
Fjolla Qerimi <qerimifjolla7@gmail.com>
2026-09-01 10:10:52 +0200

    fix: correct variable assignment for sg in word2vec_model function

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -28,16 +28,17 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     # gensim utilise sg=0 pour CBOW et sg=1 pour Skip-gram,
     # c'est donc l'inverse de notre booléen cbow
     if cbow:
-        cbow = 0
+        sg = 0
     else:
-        cbow = 1
+        sg = 1
 
     # En passant sentences directement au constructeur, gensim
     # construit le vocabulaire et entraîne le modèle automatiquement
-    model = gensim.models.Word2Vec(sentences=sentences,
-                                   vector_size=vector_size,
+    model = gensim.models.Word2Vec(vector_size=vector_size,
                                    min_count=min_count, window=window,
-                                   negative=negative, sg=cbow, epochs=epochs,
+                                   negative=negative, sg=sg, epochs=epochs,
                                    seed=seed, workers=workers)
-
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
